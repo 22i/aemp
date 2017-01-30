63bf000be2c12c232b58e5b384d26988b84d0a07
Unknown <pumpkin650@gmail.com>
2017-01-30 21:18:10 +0100

    added Fangrin, Phyg and Sheepuff mobs

--- init.lua
+++ init.lua
@@ -181,3 +181,90 @@ mobs:register_mob("aemp:6sprout", {
 })
 
 mobs:register_egg("aemp:6sprout", "Sprout", "sprout_inv.png", 0)
+
+mobs:register_mob("aemp:7fangrin", {
+	type = "animal",
+	passive = true,
+    runaway = true,
+    stepheight = 1.2,
+	hp_min = 30,
+	hp_max = 60,
+	armor = 150,
+    collisionbox = {-0.35, -0.01, -0.35, 0.35, 2, 0.35},
+    rotate = -180,
+	visual = "mesh",
+	mesh = "fangrin.b3d",
+	textures = {
+		{"fangrin.png"}
+    },
+	visual_size = {x=3, y=3},
+	walk_velocity = 0.6,
+	run_velocity = 2,
+	jump = true,
+	animation = {
+		speed_normal = 25,		speed_run = 50,
+		stand_start = 40,		stand_end = 80,
+		walk_start = 0,		walk_end = 40,
+		run_start = 0,		run_end = 40,
+	},
+})
+
+mobs:register_egg("aemp:7fangrin", "Fangrin", "fangrin_inv.png", 0)
+
+mobs:register_mob("aemp:8phyg", {
+	type = "animal",
+	passive = true,
+    runaway = true,
+    stepheight = 1.2,
+	hp_min = 30,
+	hp_max = 60,
+	armor = 150,
+    collisionbox = {-0.35, -0.01, -0.35, 0.35, 2, 0.35},
+    rotate = -180,
+	visual = "mesh",
+	mesh = "phyg.b3d",
+	textures = {
+		{"phyg.png"}
+    },
+	visual_size = {x=3, y=3},
+	walk_velocity = 0.6,
+	run_velocity = 2,
+	jump = true,
+	animation = {
+		speed_normal = 25,		speed_run = 50,
+		stand_start = 40,		stand_end = 80,
+		walk_start = 0,		walk_end = 40,
+		run_start = 0,		run_end = 40,
+	},
+})
+
+mobs:register_egg("aemp:8phyg", "Phyg", "phyg_inv.png", 0)
+
+mobs:register_mob("aemp:9sheepuff", {
+	type = "animal",
+	passive = true,
+    runaway = true,
+    stepheight = 1.2,
+	hp_min = 30,
+	hp_max = 60,
+	armor = 150,
+    collisionbox = {-0.35, -0.01, -0.35, 0.35, 2, 0.35},
+    rotate = -180,
+	visual = "mesh",
+	mesh = "sheepuff.b3d",
+	textures = {
+		{"sheepuff.png"}
+    },
+	visual_size = {x=3, y=3},
+	walk_velocity = 0.6,
+	run_velocity = 2,
+	jump = true,
+	animation = {
+		speed_normal = 25,		speed_run = 50,
+		stand_start = 40,		stand_end = 80,
+		walk_start = 0,		walk_end = 40,
+		run_start = 0,		run_end = 40,
+	},
+})
+
+mobs:register_egg("aemp:9sheepuff", "Sheepuff", "sheepuff_inv.png", 0)
